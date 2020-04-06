f33b8da52dbc68122d37ada60c89a1586f1ec813
Med Aduh <im.ramidem@gmail.com>
2020-04-06 19:50:13 +0800

    CS50M ReactNative project0: html+css

--- CS50M/reactNative/projects/project0/script.js
+++ CS50M/reactNative/projects/project0/script.js
@@ -1,14 +1,42 @@
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+// const classNames = {
+//   TODO_ITEM: "todo-container",
+//   TODO_CHECKBOX: "todo-checkbox",
+//   TODO_TEXT: "todo-text",
+//   TODO_DELETE: "todo-delete",
+// };
+
+const todos = [];
+
+const list = document.getElementById("todo-list");
+const todoForm = document.getElementById("todo-form");
+
+const itemCountSpan = document.getElementById("item-count");
+const uncheckedCountSpan = document.getElementById("unchecked-count");
+
+const todo = document.getElementById("addTodo");
+// console.log(list);
+
+function newTodo(e) {
+  e.preventDefault();
+
+  // generate id
+  const chars = "123456789abcdef".split("");
+  let id = [];
+  for (let i = 0; i <= chars.length; i++) {
+    let randomNums = Math.floor(Math.random() * 16);
+    id.push(chars[randomNums]);
+  }
+
+  // add to `todos` array
+  todos.push({ item: todo.value, isDone: false, id: id.join("") });
+
+  // todoForm.reset();
+
+  console.table(todos);
 }
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+// todos.map((item) => `<li><p>${item.todo}</p></li>`);
 
-function newTodo() {
-  alert('New TODO button clicked!')
-}
+// console.log(todos);
+
+todoForm.addEventListener("submit", newTodo);
