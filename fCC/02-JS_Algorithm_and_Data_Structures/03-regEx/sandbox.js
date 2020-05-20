f20aa842bc2a23b8b905d081389e39ee18262175
Med Aduh <im.ramidem@gmail.com>
2020-05-20 15:54:57 +0800

    fCC Debugging

--- fCC/02-JS_Algorithm_and_Data_Structures/03-regEx/sandbox.js
+++ fCC/02-JS_Algorithm_and_Data_Structures/03-regEx/sandbox.js
@@ -4,3 +4,15 @@ let text = "<h1>Winter is coming</h1>";
 let myRegex = /<.*?>/; // Change this line
 let result = text.match(myRegex);
 console.log(result);
+
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i <= len - 1; i++) {
+    // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
---
+++ fCC/02-JS_Algorithm_and_Data_Structures/04-Debugging/debugging.md
@@ -0,0 +1,1 @@
+## [Debugging](https://www.notion.so/ramidem/Debugging-4a27def979ca4867bc8dc32c8c254165)
